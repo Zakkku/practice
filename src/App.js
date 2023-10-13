badac4f7980cf99d0132d4d69c33003660883e89
Zach <wakuwakuzakku@gmail.com>
2023-10-13 14:18:36 +0900

    created hello text component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo_transparent.png';  
 import './App.css';
+import logo2 from './logo.svg';
+import HelloText from './HelloText';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Click below to view my
-        </p>
-        <a
-          className="App-link"
-          href="https://zach-japan.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Portfolio
-        </a>
         
+        <HelloText />
+        
+        <img className="React-logo"  src={logo2} alt="react logo"></img>
       </header>
+      
     </div>
   );
 }
---
+++ src/HelloText.js
@@ -0,0 +1,20 @@
+function HelloText() {
+    return (
+        <>
+        <h1>Hello, I'm Zach</h1>
+        <p>I work at FundastA in Japan</p>
+        <p>Click below to see my</p>
+        <a
+          className="App-link"
+          href="https://zach-japan.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Portfolio
+        </a>
+        </>
+        
+    )
+}
+
+export default HelloText;
